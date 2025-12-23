688512f0938d61026d859f2d638a0fce502603fc
fl4ko92 <55207278+fl4ko92@users.noreply.github.com>
2025-12-23 12:12:53 -0500

    tienda info

--- ventiq_admin_app/lib/screens/store_detail_screen.dart
+++ ventiq_admin_app/lib/screens/store_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 import '../config/app_colors.dart';
 import '../services/store_data_service.dart';
 
@@ -59,7 +60,18 @@ class _StoreDetailScreenState extends State<StoreDetailScreen> {
       children: [
         TileLayer(
           urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-          userAgentPackageName: 'com.example.app',
+          userAgentPackageName: 'VentIQAdmin/1.6.0 (+https://ventiq.com; contact: support@ventiq.com)',
+          tileSize: 256,
+        ),
+        RichAttributionWidget(
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(
+                Uri.parse('https://openstreetmap.org/copyright'),
+              ),
+            ),
+          ],
         ),
         MarkerLayer(
           markers: [
