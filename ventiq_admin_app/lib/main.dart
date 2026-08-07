f18dded28e16b4c034c78233edcfc1e65b6bc37c
aicemaria112 <aicemaria112@gmail.com>
2026-08-07 09:37:18 -0400

    update hr

--- ventiq_admin_app/lib/main.dart
+++ ventiq_admin_app/lib/main.dart
@@ -59,6 +59,7 @@ import 'screens/hr/hr_checkin_screen.dart';
 import 'screens/hr/hr_checkout_screen.dart';
 import 'screens/hr/hr_salary_report_screen.dart';
 import 'screens/hr/hr_worker_config_screen.dart';
+import 'screens/hr/hr_attendance_history_screen.dart';
 import 'screens/importadora/importadora_facturas_screen.dart';
 import 'screens/importadora/estados_factura_screen.dart';
 
@@ -225,6 +226,7 @@ class MyApp extends StatelessWidget {
         '/hr-checkout': (context) => const HRCheckoutScreen(),
         '/hr-salary-report': (context) => const HRSalaryReportScreen(),
         '/hr-worker-config': (context) => const HRWorkerConfigScreen(),
+        '/hr-attendance-history': (context) => const HRAttendanceHistoryScreen(),
         '/importadora-facturas': (context) => const ImportadoraFacturasScreen(),
         '/importadora-estados': (context) => const EstadosFacturaScreen(),
       },
